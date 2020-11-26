8d021cb5430fc33af80da22ee3106c4b1dbe6fa4
Gareth May <garethmay@hotmail.co.uk>
2020-11-26 11:30:34 +0000

    Tidying up

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,8 @@
-import React, {useEffect, useState, useRef} from 'react'
+import React, { useState } from 'react'
 import styles from "./component.module.scss";
-// import openMenuIcon from '../public/images/hamburger.svg'
-// import closeMenuIcon from '../public/images/close-rounded-white-18dp.svg'
-// import logo from '../public/images/logoOCD-01.svg'
 import Link from 'next/link'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import Nav from './Nav'
-import Image from 'next/image'
 
 const Header = () => {
 
@@ -82,23 +78,15 @@ const Header = () => {
       }
       <Link href="/">
         <a>
-          {/* <img 
-            src="/images/logoOCD.svg" 
-            className={styles.logo} 
-            alt="OCD Logo"
-            aria-label="Home"
-            height="32"
-            width="32"
-          /> */}
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 107.77 121.68" className={styles.logo} aria-label="Home">
-                <g id="Layer_2" data-name="Layer 2">
-                <g id="Layer_1-2" data-name="Layer 1">
-                  <path className="cls-1" d="M46.93,13.91A46.93,46.93,0,1,0,93.87,60.84,46.93,46.93,0,0,0,46.93,13.91Zm0,80a33,33,0,1,1,33-33A33,33,0,0,1,46.93,93.87Z"/>
-                  <path className="cls-2" d="M46.93,80.4a19.56,19.56,0,0,1,0-39.12V27.81a33,33,0,0,0,0,66.06Z"/>
-                  <path className="cls-2" d="M46.93,0V13.91a46.93,46.93,0,1,1,0,93.86v13.91A60.84,60.84,0,0,0,46.93,0Z"/>
-                </g>
-                </g>
-              </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 107.77 121.68" className={styles.logo} aria-label="Home">
+            <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_1-2" data-name="Layer 1">
+              <path className="cls-1" d="M46.93,13.91A46.93,46.93,0,1,0,93.87,60.84,46.93,46.93,0,0,0,46.93,13.91Zm0,80a33,33,0,1,1,33-33A33,33,0,0,1,46.93,93.87Z"/>
+              <path className="cls-2" d="M46.93,80.4a19.56,19.56,0,0,1,0-39.12V27.81a33,33,0,0,0,0,66.06Z"/>
+              <path className="cls-2" d="M46.93,0V13.91a46.93,46.93,0,1,1,0,93.86v13.91A60.84,60.84,0,0,0,46.93,0Z"/>
+            </g>
+            </g>
+          </svg>
         </a>
       </Link>
     </div>
